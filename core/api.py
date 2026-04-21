e61cabba43a872d8e6a4be9b933c97998ef04f2b
Eduardo <eduardooliveira7907@gmail.com>
2026-04-21 02:10:33 -0300

    finalizando task 2 Crud de cidades com  filtros

--- core/api.py
+++ core/api.py
@@ -1,8 +1,9 @@
 from ninja import Router
-from .models import Territorio
+from .models import Territorio, Cidade
 from django.shortcuts import  get_object_or_404
 from .schemas import TerritorioSchema
 from typing import List
+from .schemas import TerritorioSchema, CidadeIn, CidadeOut
 
 router = Router()
 
@@ -33,3 +34,32 @@ def remover_territorio(request, territorio_id: int):
     
     return {
         "sucesso": True , "mensagem": "Território removido com sucesso"}
+    
+
+@router.get("/cidades", response=List[CidadeOut])
+def listar_cidades(request, nome: str = None, territorio_id: int = None):
+    cidades = Cidade.objects.all()
+    if nome:
+        cidades = cidades.filter(nome__icontains=nome) 
+    if territorio_id:
+        cidades = cidades.filter(territorio_id=territorio_id)
+    return cidades
+
+@router.post("/cidade", response=CidadeOut)
+def criar_cidade(request, data: CidadeIn):
+    cidade =  Cidade.objects.create(codigo_ibge=data.codigo_ibge,nome = data.nome , territorio_id= data.territorio_id)
+    return  cidade
+
+@router.put("/cidades/{codigo_ibge}", response=CidadeOut)
+def editar_cidade(request, codigo_ibge: int, data : CidadeIn):
+    cidade = get_object_or_404(Cidade, codigo_ibge= codigo_ibge)
+    cidade.nome = data.nome
+    cidade.territorio_id = data.territorio_id
+    cidade.save()
+    return cidade
+
+@router.delete("/cidades/{codigo_ibge}")
+def remover_cidade(request,codigo_ibge: int):
+    cidade =  get_object_or_404(Cidade, codigo_ibge=codigo_ibge)
+    cidade.delete()
+    return{"sucesso": True, "mensagem": "Cidade removida com sucesso"}
--- core/schemas.py
+++ core/schemas.py
@@ -3,3 +3,13 @@ from ninja import Schema
 class TerritorioSchema(Schema):
     id: int = None
     nome: str
+    
+class CidadeIn(Schema):
+    codigo_ibge: int
+    nome: str
+    territorio_id: int
+    
+class  CidadeOut(Schema):
+    codigo_ibge:int
+    nome:str
+    territorio:TerritorioSchema
