78e98c9e1f0278922ec0b749a51369550b4b4cb8
Eduardo <eduardooliveira7907@gmail.com>
2026-04-21 00:28:36 -0300

    editar e remover feitos ,  crud completo de territorios concluido com sucesso

--- core/api.py
+++ core/api.py
@@ -1,5 +1,6 @@
 from ninja import Router
 from .models import Territorio
+from django.shortcuts import  get_object_or_404
 from .schemas import TerritorioSchema
 from typing import List
 
@@ -13,3 +14,22 @@ def listar_territorios(request):
 def criar_territorio(request, data: TerritorioSchema):
     territorio = Territorio.objects.create(nome=data.nome)
     return territorio
+
+
+@router.put("/territorios/{territorio_id}", response=TerritorioSchema)
+def editar_territorio(request, territorio_id: int, data: TerritorioSchema):
+    territorio = get_object_or_404(Territorio, id = territorio_id)
+    territorio.nome = data.nome
+    
+    territorio.save()
+    
+    return territorio
+
+@router.delete("/territorios/{territorio_id}")
+def remover_territorio(request, territorio_id: int):
+    territorio = get_object_or_404(Territorio, id=territorio_id)
+    
+    territorio.delete()
+    
+    return {
+        "sucesso": True , "mensagem": "Território removido com sucesso"}
